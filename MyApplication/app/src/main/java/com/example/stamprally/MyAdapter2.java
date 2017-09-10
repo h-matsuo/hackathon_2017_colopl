07f6d3a13e7206ba138f1f2e30a9a7f05c31270a
koutaNakanishi <nizi26i26@gmail.com>
2017-09-10 23:18:53 +0900

    スタンプカードを実装しました

--- MyApplication/app/src/main/java/com/example/stamprally/MyAdapter2.java
+++ MyApplication/app/src/main/java/com/example/stamprally/MyAdapter2.java
@@ -1,5 +1,12 @@
 package com.example.stamprally;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +15,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import static com.example.stamprally.R.id.image;
+import static java.security.AccessController.getContext;
 
 /**
  * Created by mb-347 on 2017/09/10.
@@ -19,12 +29,19 @@ public class MyAdapter2     extends RecyclerView.Adapter<MyAdapter2.ItemViewHold
     public static class ItemViewHolder extends RecyclerView.ViewHolder {
 
         public TextView mTextView;
-        public ImageView mImageView;
+        public ImageView mImageView[]=new ImageView[8];
 
         public ItemViewHolder(View v){
             super(v);
             mTextView = (TextView)v.findViewById(R.id.list_item_text2);
-            mImageView=(ImageView)v.findViewById(R.id.imageView2);
+            mImageView[0]=(ImageView)v.findViewById(R.id.image1);
+            mImageView[1]=(ImageView)v.findViewById(R.id.image2);
+            mImageView[2]=(ImageView)v.findViewById(R.id.image3);
+            mImageView[3]=(ImageView)v.findViewById(R.id.image4);
+            mImageView[4]=(ImageView)v.findViewById(R.id.image5);
+            mImageView[5]=(ImageView)v.findViewById(R.id.image6);
+            mImageView[6]=(ImageView)v.findViewById(R.id.image7);
+            mImageView[7]=(ImageView)v.findViewById(R.id.image8);
         }
     }
 
@@ -50,7 +67,12 @@ public class MyAdapter2     extends RecyclerView.Adapter<MyAdapter2.ItemViewHold
     @Override
     public void onBindViewHolder(MyAdapter2.ItemViewHolder holder, int position) {
         holder.mTextView.setText(stampCards.get(position).cardName);
-        holder.mImageView.setImageResource(stampCards.get(position).cardId[0]);
+
+        for(int i=0;i<stampCards.get(position).num;i++){//スタンプの数だけ画像を設定
+            Bitmap bitmap =((BitmapDrawable)holder.mImageView[i].getDrawable()).getBitmap();
+
+        holder.mImageView[i].setImageResource(stampCards.get(position).cardId[i]);
+        }
 
     }
 
